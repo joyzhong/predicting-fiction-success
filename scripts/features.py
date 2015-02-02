ad3268be2b9e3e8d40af9b243c3d23f0ba0d8085
Joy Zhong <joytzhong@gmail.com>
2015-02-02 17:21:19 -0500

    Get avg sentence length in characters

--- scripts/features.py
+++ scripts/features.py
@@ -1,18 +1,24 @@
+from __future__ import division
 import re
-import nltk.data
+import pickle
 
 # Input: file object
 # Returns as a tuple the avg sentence length in characters and in words
-def getAvgSentenceLength(f):
-	tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-	data = f.read()
-	print '\n-----\n'.join(tokenizer.tokenize(data))
-
-
-# Input: file name
-def getFeatures(filename):
+def getAvgSentenceLength(filename):
 	f = open(filename, 'r')
-	getAvgSentenceLength(f)
+
+	# Split by sentences
+	segmenter_file = open('english.pickle', 'r')
+	sentence_segmenter = pickle.Unpickler(segmenter_file).load()
+	sentences = sentence_segmenter.tokenize(f.read())
+
+	lengthInChars = 0
+	for i, sentence in enumerate(sentences):
+		lengthInChars += len(sentence)
+
+	print "Average sentence length in chars: " + str(lengthInChars / len(sentences))
 	f.close()
-	
-getFeatures("../novels/Historical_Fiction/hf_fold1/success1/1880.txt")
+
+	return lengthInChars / len(sentences)
+
+getAvgSentenceLength("../novels/Historical_Fiction/hf_fold1/success1/1880.txt")

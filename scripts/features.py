511fa20ac1021d908a5317aedd92f90a4c7ecad0
Joy Zhong <joytzhong@gmail.com>
2015-02-02 21:03:55 -0500

    Added POS tagger

--- scripts/features.py
+++ scripts/features.py
@@ -4,6 +4,11 @@ import argparse
 import pickle
 import nltk
 
+# https://github.com/sloria/textblob-aptagger/tree/master
+# https://honnibal.wordpress.com/2013/09/11/a-good-part-of-speechpos-tagger-in-about-200-lines-of-python/
+from textblob import TextBlob
+from textblob_aptagger import PerceptronTagger
+
 # Input: file object
 # Returns as a tuple the avg sentence length in characters and in words
 def getAvgSentenceLength(filename):
@@ -13,19 +18,18 @@ def getAvgSentenceLength(filename):
 	tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 	sentences = tokenizer.tokenize(f.read())
 
-	lengthInChars = 0
-	lengthInWords = 0
-	for i, sentence in enumerate(sentences):
-		lengthInChars += len(sentence)
+	totCharLength = 0
+	totWords = 0
+	for sentence in sentences:
+		totCharLength += len(sentence)
 		words = nltk.word_tokenize(sentence)
+		totWords += len(words)
 
-	lengthInChars /= len(sentences)
-	lengthInWords /= len(sentences)
-	print "Average sentence length in chars: " + str(lengthInChars)
-	print "Average sentence length in words: " + str(lengthInWords)
+	print "Average sentence length in chars: " + str(totCharLength / len(sentences))
+	print "Average sentence length in words: " + str(totWords / len(sentences))
 	f.close()
 
-	return lengthInChars, lengthInWords
+	return totCharLength / len(sentences), totWords / len(sentences)
 
 def getAvgWordLengthNLTK(filename):
 	f = open(filename, 'r')
@@ -57,14 +61,18 @@ def getAvgWordLength(filename):
 
 	return totLength / numWords
 
+def getPosTags(filename):
+	f = open(filename, 'r')
+	text = TextBlob(f.read(), pos_tagger=PerceptronTagger())
+	f.close()
+	return text.tags
+
 # Input: file name
 def getFeatures(filename):
-	# f = open(filename, 'r')
-	# getAvgSentenceLength(f)
-	# f.close()
-	print getAvgSentenceLength(filename)
-	print "Average word length: " + str(getAvgWordLength(filename))
-	print "Average word length (NLTK): " + str(getAvgWordLengthNLTK(filename))
+	# print getAvgSentenceLength(filename)
+	# print "Average word length: " + str(getAvgWordLength(filename))
+	# print "Average word length (NLTK): " + str(getAvgWordLengthNLTK(filename))
+	# print "POS Tags: " + getPosTags(filename)
 	
 def main():
 	# getFeatures("../novels/Historical_Fiction/hf_fold1/success1/1880.txt")

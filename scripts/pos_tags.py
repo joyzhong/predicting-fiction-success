f8b61edbd88736dd4951d3b2364af3ac8af6cd48
Joy Zhong <joytzhong@gmail.com>
2015-02-08 22:38:00 -0500

    Stored all POS tag counts as pickles for all data

--- scripts/pos_tags.py
+++ scripts/pos_tags.py
@@ -6,21 +6,22 @@ import pickle
 # Iterate through subdirectories of directory and generate POS tags for
 # all files in each subdirectory
 # Meant to be run from genre folder, e.g. "Adventure_Stories"
+# Get rid of unicode: [^\x00-\x7F]
 def getPosTags(root_dir):
 	for subdir, dirs, files in os.walk(root_dir):
 		for f in files:
-			if ".txt" in f:
+			if ".txt" in f and f.replace(".txt", ".pkl") not in files:
 				f_in = os.path.join(subdir, f)
 				print f_in
 				saveTagsAsPickle(f_in)
 
 def saveTagsAsPickle(file_in):
 	f = open(file_in, 'r')
-	tags = features.getPosTags(f.read())
+	counts = features.getPosCountPerSentence(file_in)
 	f.close()
 
 	f_out = open(file_in.replace(".txt", ".pkl"), 'wb')
-	pickle.dump(tags, f_out)
+	pickle.dump(counts, f_out)
 	f_out.close()
 
 def loadTagsFromPickle(pickle_in):
@@ -29,5 +30,6 @@ def loadTagsFromPickle(pickle_in):
 	pkl_file.close()
 	return tags
 
-# getPosTags(sys.argv[1])
-saveTagsAsPickle(sys.argv[1])
+getPosTags(sys.argv[1])
+# saveTagsAsPickle(sys.argv[1])
+
